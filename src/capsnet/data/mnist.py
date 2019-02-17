66f62d489879bdd95a8a75f4bcdd592de79db10b
Jihoon Lee <jihoon.lee@kakaobrain.com>
2019-02-17 17:03:41 +0900

    Add mnist dataset

--- src/capsnet/data/mnist.py
+++ src/capsnet/data/mnist.py
@@ -2,31 +2,21 @@ import os
 from torchvision import datasets, transforms
 
 from ..utils.logger import logger
+from ..utils.config import cfg
 
 dataset = {'train': None, 'test': None}
-
-DATA_ROOT = '/data/public/rw/datasets'
-IN_PLACE = '../data/mnist'
 
 preprocess = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5,), (1.0,))])
 
-def is_braincloud():
-    return os.path.exists(DATA_ROOT)
 
 def mnist():
     global dataset
 
-    logger.debug('Loading MNIST...')
+    datapath = cfg.DATASET.PATH
+    logger.debug('Loading MNIST into {}'.format(datapath))
 
-    is_cloud = is_braincloud()
-
-    if is_cloud:
-        logger.debug('- In brain cloud')
-
-    datapath = os.path.join(DATA_ROOT,
-                            'mnist') if is_cloud else IN_PLACE
     dataset['train'] = datasets.MNIST(
         datapath, train=True, download=True, transform=preprocess)
 
