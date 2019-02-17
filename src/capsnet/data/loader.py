c73d0d0ccb96aa0e710328481c83c6ccdff527bd
Jihoon Lee <jihoon.lee@kakaobrain.com>
2019-02-17 17:03:56 +0900

    Add dataloader

--- src/capsnet/data/loader.py
+++ src/capsnet/data/loader.py
@@ -1,11 +1,28 @@
 from torch.utils.data import DataLoader
+from ..utils.config import cfg
+from ..utils.logger import logger
 
-NUM_WORKERS = 8
 
-def train_loader(dataset, batch_size):
+def train(dataset):
+    logger.debug(
+        'Train Loader - Batch: {}, pin_memory: {}, Num Worker: {}'.
+        format(cfg.TRAIN.BATCH_SIZE, cfg.GPU.USE, cfg.DATASET.NUM_WORKERS))
     return DataLoader(
         dataset,
-        batch_size=batch_size,
-        pin_memory=True,
+        batch_size=cfg.TRAIN.BATCH_SIZE,
+        pin_memory=cfg.GPU.USE,
         shuffle=True,
-        num_workers=NUM_WORKERS)
+        num_workers=cfg.DATASET.NUM_WORKERS)
+
+
+def test(dataset):
+    logger.debug(
+        'Test Loader  - Batch: {}, pin_memory: {}, Num Worker: {}'.
+        format(cfg.TRAIN.BATCH_SIZE, cfg.GPU.USE, cfg.DATASET.NUM_WORKERS))
+
+    return DataLoader(
+        dataset,
+        batch_size=cfg.TEST.BATCH_SIZE,
+        pin_memory=cfg.GPU.USE,
+        shuffle=False,
+        num_workers=cfg.DATASET.NUM_WORKERS)
