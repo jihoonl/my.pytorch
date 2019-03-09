184d73a3a355acc768e5ba0a106377b93631d723
Jihoon Lee <jihoonlee.in@gmail.com>
2019-03-09 19:29:20 +0900

    log in time directory (#15)

--- src/mytorch/utils/tensorboard.py
+++ src/mytorch/utils/tensorboard.py
@@ -1,7 +1,12 @@
 
+import os
 
 from tensorboardX import SummaryWriter
 
-def get_writer(cfg):
-    writer = SummaryWriter(cfg.LOG_DIR)
+def get_writer(time, cfg):
+    dirpath = os.path.join(cfg.LOG_DIR, time)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+
+    writer = SummaryWriter(dirpath)
     return writer
